64a6c2ef86a87932296d24f64ba2cdbc2146f227
RogerDariva <111013274+RogerDariva@users.noreply.github.com>
2023-10-03 21:23:45 -0300

    Add files via upload

---
+++ Sap_FabricaAutomotiva/Area_Fabrica.php
@@ -0,0 +1,46 @@
+<?php
+
+include('conexao_Fabrica.php');
+
+
+$query = $dbh->prepare('SELECT area, COUNT(*) AS total FROM alocacao GROUP BY area;');
+
+$query->execute();
+
+$totalAreas = $query->fetchAll();
+
+echo '<pre>';
+print_r ($totalAreas);
+echo '</pre>'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilo_fabrica.css">
+</head>
+<body>
+
+    <div class="divMain"> </div>
+
+    <?php
+
+    for($area = 1; $area < 12; $area++){
+        //print_r($totalAreas[$area]['total']);
+        echo '<a href="detalhes_Area.php?idArea='.$area.'">';
+            if(isset($totalAreas[$area]['total'])){
+                echo '<div class="div'.$area.' azul">';
+            }
+            else{
+                echo '<div class="div'.$area.' branco">';
+            }
+                echo '<p style="text-align: center;">'.$area.'</p>';
+            echo'</div>
+        </a>';
+        }
+    ?>
+</body>
+</html>
--- Sap_FabricaAutomotiva/detalhes_Area.php
+++ Sap_FabricaAutomotiva/detalhes_Area.php
@@ -1,17 +1,51 @@
 <?php
-    include('conexao_Fabrica.php')
+    include('conexao_Fabrica.php');
+
+    $area = $_GET['idArea'];
+
+    $query = $dbh->prepare('SELECT area, modelo, preco FROM alocacao as al 
+    INNER JOIN automoveis as a ON a.id=al.automovel WHERE area =:area');
+    
+    $query->execute(array(':area'=> $area));
+
+    $automoveis = $query->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalhes de Área</title>
-</head>
-<body>
-    <div class = "divMain">
-        Teste
-    </div>
-</body>
+<html>
+    <head>
+        <title>Automoveis por Área</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <style>
+            table{
+                text-align:left;
+            }
+            tr:nth-child(even){
+                background-color: pink;
+                color:white;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Area <?php echo $area ?></h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Modelo</th>
+                <th>Preço</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($automoveis as $automovel){
+                        echo '<tr>';
+                        echo '<td>'.$automovel['modelo'].'</td>';
+                        echo '<td>'.$automovel['preco'].'</td>';
+                        echo '</tr>';
+                    }
+                ?>
+            </tbody>
+        </table>
+    </body>
 </html>
